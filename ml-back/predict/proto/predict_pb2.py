31a671cb98530e16def56f8c9dd7234c932c9126
ivan <6_6_6_66@mail.ru>
2025-01-17 00:28:48 +0300

    refactor: :recycle: new version generated gRPC and protobuf files

--- ml-back/predict/proto/predict_pb2.py
+++ ml-back/predict/proto/predict_pb2.py
@@ -24,17 +24,19 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rpredict.proto\x12\x07predict\"&\n\x10PredictorRequest\x12\x12\n\nimage_data\x18\x01 \x01(\x0c\" \n\x0ePredictorReply\x12\x0e\n\x06result\x18\x01 \x01(\t2L\n\tPredictor\x12?\n\x07Predict\x12\x19.predict.PredictorRequest\x1a\x17.predict.PredictorReply\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rpredict.proto\x12\x07predict\"E\n\x10PredictorRequest\x12\x12\n\nimage_data\x18\x01 \x03(\x0c\x12\x1d\n\x05plant\x18\x02 \x01(\x0e\x32\x0e.predict.Plant\" \n\x0ePredictorReply\x12\x0e\n\x06result\x18\x01 \x03(\t*\x8d\x01\n\x05Plant\x12\x10\n\x0cPLANT_TOMATO\x10\x00\x12\x12\n\x0ePLANT_CUCUMBER\x10\x01\x12\x0f\n\x0bPLANT_SALAD\x10\x02\x12\x0f\n\x0bPLANT_MELON\x10\x03\x12\x14\n\x10PLANT_WATERMELON\x10\x04\x12\x14\n\x10PLANT_STRAWBERRY\x10\x05\x12\x10\n\x0cPLANT_PEPPER\x10\x06\x32L\n\tPredictor\x12?\n\x07Predict\x12\x19.predict.PredictorRequest\x1a\x17.predict.PredictorReply\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'predict_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
+  _globals['_PLANT']._serialized_start=132
+  _globals['_PLANT']._serialized_end=273
   _globals['_PREDICTORREQUEST']._serialized_start=26
-  _globals['_PREDICTORREQUEST']._serialized_end=64
-  _globals['_PREDICTORREPLY']._serialized_start=66
-  _globals['_PREDICTORREPLY']._serialized_end=98
-  _globals['_PREDICTOR']._serialized_start=100
-  _globals['_PREDICTOR']._serialized_end=176
+  _globals['_PREDICTORREQUEST']._serialized_end=95
+  _globals['_PREDICTORREPLY']._serialized_start=97
+  _globals['_PREDICTORREPLY']._serialized_end=129
+  _globals['_PREDICTOR']._serialized_start=275
+  _globals['_PREDICTOR']._serialized_end=351
 # @@protoc_insertion_point(module_scope)
