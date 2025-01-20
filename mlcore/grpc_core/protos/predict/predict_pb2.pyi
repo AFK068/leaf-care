9c01df6d4c356a9e644f0b408e64a2d8cc122097
ivan <6_6_6_66@mail.ru>
2025-01-20 20:33:54 +0300

    refactor: :recycle: generated gRPC and protobuf files

--- mlcore/grpc_core/protos/predict/predict_pb2.pyi
+++ mlcore/grpc_core/protos/predict/predict_pb2.pyi
@@ -2,7 +2,7 @@ from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Optional as _Optional, Union as _Union
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -23,6 +23,20 @@ PLANT_WATERMELON: Plant
 PLANT_STRAWBERRY: Plant
 PLANT_PEPPER: Plant
 
+class ClassProbability(_message.Message):
+    __slots__ = ("class_name", "probability")
+    CLASS_NAME_FIELD_NUMBER: _ClassVar[int]
+    PROBABILITY_FIELD_NUMBER: _ClassVar[int]
+    class_name: str
+    probability: float
+    def __init__(self, class_name: _Optional[str] = ..., probability: _Optional[float] = ...) -> None: ...
+
+class ImageResults(_message.Message):
+    __slots__ = ("results",)
+    RESULTS_FIELD_NUMBER: _ClassVar[int]
+    results: _containers.RepeatedCompositeFieldContainer[ClassProbability]
+    def __init__(self, results: _Optional[_Iterable[_Union[ClassProbability, _Mapping]]] = ...) -> None: ...
+
 class PredictorRequest(_message.Message):
     __slots__ = ("image_data", "plant")
     IMAGE_DATA_FIELD_NUMBER: _ClassVar[int]
@@ -34,5 +48,5 @@ class PredictorRequest(_message.Message):
 class PredictorReply(_message.Message):
     __slots__ = ("result",)
     RESULT_FIELD_NUMBER: _ClassVar[int]
-    result: _containers.RepeatedScalarFieldContainer[str]
-    def __init__(self, result: _Optional[_Iterable[str]] = ...) -> None: ...
+    result: _containers.RepeatedCompositeFieldContainer[ImageResults]
+    def __init__(self, result: _Optional[_Iterable[_Union[ImageResults, _Mapping]]] = ...) -> None: ...
