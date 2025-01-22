29cd2e80a26576e1a3d05bc2256457bdc22ab86a
Ivan Gobets <6_6_6_66@mail.ru>
2025-01-22 23:57:50 +0300

    chore: :bulb: add comments in grpc server

--- mlcore/grpc_core/servers/manager.py
+++ mlcore/grpc_core/servers/manager.py
@@ -1,4 +1,3 @@
-import os
 from concurrent import futures
 
 import grpc
@@ -12,32 +11,63 @@ class Server:
     """
     Server class is a gRPC server manager.
 
-    The `register` method is responsible for registering the gRPC services with the server.
+    It initializes the gRPC server, registers services, and provides methods
+    to start and stop the server.
     """
     def __init__(self) -> None:
-        # Load the environment variables.
+        """
+        Initializes the gRPC server.
+
+        - Loads environment variables using `dotenv`.
+        - Sets the server address from settings.
+        - Creates a gRPC server with a thread pool executor.
+        """
+        # Load environment variables from .env file.
         load_dotenv()
+
+        # Set the server address using settings.
         self.server_address = f"{settings.GRPC_HOST_LOCAL}:{settings.GRPC_PORT}"
 
-        # Create the gRPC server.
+        # Create a gRPC server with a thread pool executor.
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+        # Bind the server to the specified address.
         self.server.add_insecure_port(self.server_address)
     
     def register(self) -> None:
-        # Register the services.
+        """
+        Registers gRPC services with the server.
+
+        - Adds the `PredictService` to the server.
+        """
+        # Register the PredictService with the server.
         predict_pb2_grpc.add_PredictorServicer_to_server(
             PredictService(), self.server
         )
     
     def run(self) -> None:
-        # Start the server.
+        """
+        Starts the gRPC server.
+
+        - Registers services.
+        - Starts the server and waits for termination.
+        """
+        # Register services before starting the server.
         self.register()
         
+        # Start the server.
         self.server.start()
+
+        # Keep the server running until termination.
         self.server.wait_for_termination()
 
 
     def stop(self) -> None:
-        # Stop the server.
+        """
+        Stops the gRPC server.
+
+        - Stops the server immediately.
+        """
+        # Stop the server without waiting for ongoing requests to complete
         self.server.stop(grace=False)
     
