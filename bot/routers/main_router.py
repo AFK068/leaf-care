0560c394c97994066ba600e4558697d24ff554ca
Ivan Gobets <6_6_6_66@mail.ru>
2025-04-10 00:01:20 +0300

    perf: :zap: add new router

--- bot/routers/main_router.py
+++ bot/routers/main_router.py
@@ -1,10 +1,11 @@
 from aiogram import Router
 
-from bot.handlers import base_router, predict_router
+from bot.handlers import base_router, default_router, predict_router
 
 main_router = Router()
 
 main_router.include_routers(
     base_router,
     predict_router,
+    default_router,
 )
